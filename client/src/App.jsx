30a45b2d00c2278d8b0b9631e97b3cfa80a421c4
Victor-Okenwa <okenwavictor003@gmail.com>
2026-04-06 20:36:52 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Container } from "semantic-ui-react";
+import "./App.css";
+import TodoList from "./TodoList";
+
+
+function App() {
+  return (
+    <div>
+      <Container>
+        <TodoList />
+      </Container>
+    </div>
+    
+  )
+}
+
+export default App
