9063c3c4f2e218d959889153439ec2400da614ca
Victor-Okenwa <okenwavictor003@gmail.com>
2026-04-27 21:57:36 +0100

    update origin

--- server/internal/middleware/cors.go
+++ server/internal/middleware/cors.go
@@ -1,17 +1,27 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 // CORSMiddleware adds CORS headers so your React frontend can communicate with the Go backend
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Allow requests from localhost:5173 (Vite dev server)})
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, OPTIONS, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		origin := r.Header.Get("Origin")
+
+		allowedOrigin := "https://go-todo-client.vercel.app" // Change to your real frontend URL
+		if r.Host == "localhost:9000" || os.Getenv("ENVIRONMENT") == "development" {
+			allowedOrigin = origin
+		}
+
+		w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		// Handle preflight OPTIONS requests
+		// Handle preflight OPTIONS request
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
 			return
